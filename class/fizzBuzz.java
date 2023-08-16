3671cc389d94817ed56c2de2f3575defaaff6a25
Muhammad Yousuf Khan <khanyousuf2144@gmail.com>
2023-08-16 16:36:00 +0530

    fizzBuzz problem

---
+++ class/fizzBuzz.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class fizzBuzz {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = input.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 15 == 0) {
+                System.out.println("Fizz Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
