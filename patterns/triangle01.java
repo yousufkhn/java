bf71ce21d44d835979b1301e8575a2d90a4b8cd3
Muhammad Yousuf Khan <khanyousuf2144@gmail.com>
2023-08-19 13:47:06 +0530

    triangle01 pattern done

---
+++ patterns/triangle01.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class triangle01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int height = input.nextInt();
+        int toggle = 1;
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(toggle + " ");
+                toggle = (toggle == 0 ? 1 : 0);
+            }
+            System.out.println();
+        }
+    }
+}
