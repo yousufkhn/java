8077f25b1c4cf323547247add4b8648a195a69fb
Muhammad Yousuf Khan <khanyousuf2144@gmail.com>
2023-08-19 13:22:12 +0530

    halfPyramidWithNumbers pattern done

---
+++ patterns/halfPyramidWithNumbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class halfPyramidWithNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int height = input.nextInt();
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
