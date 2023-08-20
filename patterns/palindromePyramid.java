782e4ba1a2a65c963a70ae9c6fc857f646581e34
Muhammad Yousuf Khan <khanyousuf2144@gmail.com>
2023-08-20 17:57:38 +0530

    palindromePyramid pattern done

---
+++ patterns/palindromePyramid.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class palindromePyramid {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j > 0; j--) {
+                System.out.print(j);
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
