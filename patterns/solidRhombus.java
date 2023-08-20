3a37856f06bc938599bfbdefce57489df73b1108
Muhammad Yousuf Khan <khanyousuf2144@gmail.com>
2023-08-20 17:20:41 +0530

    solidRhombus pattern done

---
+++ patterns/solidRhombus.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class solidRhombus {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n; j++) {
+                System.out.print("X");
+            }
+            System.out.println();
+        }
+    }
+}
